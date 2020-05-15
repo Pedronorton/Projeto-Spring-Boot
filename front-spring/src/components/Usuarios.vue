239d368279106a151c83e79940ca67916a44c381
Pedro <p.paiva.pdo@gmail.com>
2020-05-15 15:23:00 -0300

    tabela get clientes implementado edit e delete

--- front-spring/src/components/Usuarios.vue
+++ front-spring/src/components/Usuarios.vue
@@ -2,118 +2,52 @@
   <div>
     <Navbar id="teste" class="navb" />
     <Alert class="teste" :message="alertBody.message" :type="alertBody.type" :visible="visible" />
-    <b-modal id="modal-edit" title="Editar Cliente" @ok="handleEdit">
-        <form ref="form">
-        <b-form-group
-          label="Nome"
-        >
-          <b-form-input
-            disabled
-            v-model="editedItem.nome"
-          ></b-form-input>
-        </b-form-group>
 
-        <b-form-group
-          disabled
-          label="CPF/CNPJ"
-        >
-          <b-form-input
-            disabled
-            v-model="editedItem.cpfOuCnpj"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          label="Email"
-        >
-          <b-form-input
-            v-model="editedItem.email"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          label="Telefone"
-        >
-          <b-form-input
-            disabled
-            v-model="editedItem.telefone1"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          label="Logradouro"
-        >
-          <b-form-input
-            disabled
-            v-model="editedItem.logradouro"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          label="Cep"
-        >
-          <b-form-input
-            disabled
-            v-model="editedItem.cep"
-          ></b-form-input>
-        </b-form-group>
-
-        <fieldset class="form-group">
-          <div class="row">
-            <legend class="col-form-label col-sm-2 pt-0">Tipo</legend>
-            <div class="col-sm-10">
-              <div class="form-check">
-                <div>
-                  <b-form-radio-group
-                    v-model="selected"
-                    :options="options"
-                    class="mb-3"
-                    value-field="item"
-                    text-field="name"
-                  ></b-form-radio-group>
-                </div>
-              </div>
-            </div>
-          </div>
-        </fieldset>
-      </form>
-      </b-modal>
+    <b-modal id="delete-modal" title="Tem certeza que deseja deletar o item ?" @ok="handleDelete()">
+    </b-modal>
 
     <div class="container-list">
       <h1>Lista de Clientes</h1>
       <div class="list-categoria">
-        <ul class="list-group">
-          <li
-            class="list-group-item d-flex justify-content-between align-items-center element-categoria"
-            v-for="(cliente,index) in tableData"
-            v-bind:key="index"
-          >
-            <h6>{{cliente.nome}}</h6>
-            <div class="icons">
-              <b-icon
-                class="icon-delete"
-                icon="trash-fill"
-                aria-hidden="true"
-                v-b-modal="'modal-delete'"
-              ></b-icon>
 
-              <b-modal
-                id="modal-delete"
-                title="Tem certeza que deseja deletar"
-                @ok="handleDelete(cliente)"
-              >
-                <p class="my-4"></p>
-              </b-modal>
+        <b-table :items="tableData" :fields="fields" striped responsive="sm" :busy="isBusy">
+          <template v-slot:cell(opções)="row">
+            <b-icon class="icon" icon="trash-fill" aria-hidden="true" @click="showDeleteModal(row.item)" ></b-icon>
+            <b-icon class="icon" icon="pencil" aria-hidden="true" @click="row.toggleDetails"></b-icon>
+          </template>
+    
+          <template v-slot:row-details="row" v-slot:table-busy>
+            <b-card>
+              <b-form>
+                <h1>Editar item</h1>
+                <b-form-group>
+                  <b-input v-model="row.item.nome" disabled></b-input>
+                </b-form-group>
 
-              <b-icon
-                icon="pencil"
-                aria-hidden="true"
-                v-b-modal="'modal-edit'"
-                @click="handleSubmitEdit(cliente)"
-              ></b-icon>
-            </div>
-          </li>
-        </ul>
+                <b-form-group>
+                  <b-input v-model="row.item.email"></b-input>
+                </b-form-group>
+
+                <b-form-group>
+                  <b-input v-model="row.item.cpfOuCnpj" disabled></b-input>
+                </b-form-group>
+
+                <b-form-group>
+                  <b-input v-model="row.item.telefone" disabled></b-input>
+                </b-form-group>
+
+                <b-form-group>
+                  <b-input v-model="row.item.logradouro" disabled></b-input>
+                </b-form-group>
+
+                <b-form-group>
+                  <b-input v-model="row.item.cep" disabled></b-input>
+                </b-form-group>
+              </b-form>
+              <b-button size="sm" @click="handleEdit(row.item)" variant="primary">Salvar</b-button>
+            </b-card>
+          </template>
+        </b-table>
       </div>
     </div>
   </div>
@@ -126,13 +60,25 @@ import Alert from "../components/utils/Alert";
 export default {
   data() {
     return {
+      fields: [
+        "id",
+        "nome",
+        "email",
+        "cpfOuCnpj",
+        "telefone",
+        "logradouro",
+        "cep",
+        "opções"
+      ],
+      isBusy: false,
       tableData: [],
-      editedItem:{
+      editedItem: {
+        id:"",
         nome: "",
         cpfOuCnpj: "",
         email: "",
         tipo: null,
-        telefone1: "",
+        telefone: "",
         logradouro: "",
         numero: "99",
         cep: "",
@@ -162,17 +108,18 @@ export default {
 
   async created() {
     try {
+      this.isBusy = true
       const res = await Clientes.getAll();
-      console.log(res);
-      
+
       if (Object.keys(res.data).length != 0) {
         res.data.forEach(element => {
+          
           const temp = {
             id: element.id,
             nome: element.nome,
             email: element.email,
             cpfOuCnpj: element.cpfOuCnpj,
-            telefone1: element.telefones[0],
+            telefone: element.telefones[0],
             logradouro: element.enderecos[0].logradouro,
             cep: element.enderecos[0].cep
           };
@@ -204,14 +151,19 @@ export default {
         this.visible = null;
       }, 5000);
     }
+    this.isBusy = false
   },
   methods: {
+    showDeleteModal(item){
+      this.deletedItem = item
+      this.$bvModal.show("delete-modal");
+    },
+
     showAlert(message, type, error) {
       const temp = {
         message: message + ": " + error,
         type: type
       };
-      
 
       this.alertBody = temp;
       this.visible = 5;
@@ -221,10 +173,14 @@ export default {
       }, 5000);
     },
 
-    async handleDelete(item) {
-      this.$bvModal.hide("modal-delete"); // proprio do componente
+    async handleDelete() {
+      
+      this.$bvModal.hide("delete-modal"); // proprio do componente
+      
       try {
-        await Clientes.del(item.id);
+        await Clientes.del(this.deletedItem.id);
+        const index = this.tableData.indexOf(this.deletedItem)
+        this.tableData.splice(index,1)
 
         const temp = {
           message: "Cliente deletado com sucesso !",
@@ -249,20 +205,17 @@ export default {
         }, 5000);
       }
     },
-    handleSubmitEdit(item){
-      this.editedItem = item
-    },
-    async handleEdit() {
-      
-      const from = {
-        id: this.editedItem.id,
-        nome: this.editedItem.nome,
-        email: this.editedItem.email,
-      }
 
-      try{
-        await Clientes.put(from.id, from)
-        
+    async handleEdit(item) {
+      const from = {
+        id: item.id,
+        nome: item.nome,
+        email: item.email
+      };
+
+      try {
+        await Clientes.put(from.id, from);
+
         const temp = {
           message: "Cliente atualizado com sucesso !",
           type: "primary"
@@ -273,11 +226,9 @@ export default {
         setTimeout(function() {
           this.visible = null;
         }, 5000);
-
-      }
-      catch(e){
+      } catch (e) {
         const temp = {
-          message: "Erro ao atualizar o cliente: "+e.message,
+          message: "Erro ao atualizar o cliente: " + e.message,
           type: "danger"
         };
 
@@ -286,7 +237,6 @@ export default {
         setTimeout(function() {
           this.visible = null;
         }, 5000);
-
       }
     }
   }
@@ -315,7 +265,7 @@ export default {
 .icons {
   left: 0;
 }
-.icon-delete {
+.icon {
   margin-right: 10px;
 }
 </style>
