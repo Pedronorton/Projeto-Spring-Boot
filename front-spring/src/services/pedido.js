3ccfca3e5c2b043a3b41be09707cbad0fedc8b7f
Pedro <p.paiva.pdo@gmail.com>
2020-05-14 21:23:33 -0300

    implementado getAll tabela pedidos

--- front-spring/src/services/pedido.js
+++ front-spring/src/services/pedido.js
@@ -1,10 +1,15 @@
 import Api from "./config"
 
 
+const getAll = () => {
+    return Api().get('/pedidos')
+}
+
 const post = (data) => {
     return Api().post('/pedidos', data)
 }
 
 export default {
     post,
+    getAll,
 }
