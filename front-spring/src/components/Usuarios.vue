0ea12a345d1ff88297b75ea4fc627c6fb21bd5ae
Pedro <p.paiva.pdo@gmail.com>
2020-05-21 16:01:05 -0300

    reestruturação: criada a view SaveUsuarioModal

--- front-spring/src/components/Usuarios.vue
+++ front-spring/src/components/Usuarios.vue
@@ -4,99 +4,14 @@
 
     <b-modal id="delete-modal" title="Tem certeza que deseja deletar o item ?" @ok="handleDelete()"></b-modal>
 
-    <!-- MODAL SAVE -->
-    <b-modal id="modal-save" title="Cadastrar" @ok="handleSave()">
-      <p class="my-4"></p>
-
-      <form>
-        <div class="form-group row">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Nome</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" v-model="formUser.nome" />
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">CPF/CNPJ</label>
-          <div class="col-sm-10">
-            <input class="form-control" v-model="formUser.cpfOuCnpj" />
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label for="inputPassword3" class="col-sm-2 col-form-label">Email</label>
-          <div class="col-sm-10">
-                        <input class="form-control" v-model="formUser.email" />
-
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Telefone</label>
-          <div class="col-sm-10">
-            <input class="form-control" v-model="formUser.telefone1" />
-
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Cidade</label>
-          <div class="col-sm-10">
-            <b-form-input placeholder="Selecione uma cidade" list="my-list-id" v-model="formUser.cidade"></b-form-input>
-
-            <datalist id="my-list-id">
-              <option v-for="(cidade,index) in tableDataCidade" v-bind:key="index">{{ cidade.nome }}</option>
-            </datalist>
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Logradouro</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" v-model="formUser.logradouro" />
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Cep</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" v-model="formUser.cep" />
-          </div>
-        </div>
-
-        <fieldset class="form-group">
-          <div class="row">
-            <legend class="col-form-label col-sm-2 pt-0">Tipo</legend>
-            <div class="col-sm-10">
-              <div class="form-check">
-                <div>
-                  <b-form-radio-group
-                    v-model="selected"
-                    :options="options"
-                    class="mb-3"
-                    value-field="item"
-                    text-field="name"
-                  ></b-form-radio-group>
-                </div>
-              </div>
-            </div>
-          </div>
-        </fieldset>
-        <div class="form-group row">
-          <div class="col-sm-10"></div>
-        </div>
-      </form>
-    </b-modal>
-<!-- --------------------------------------- -->
-
+    <SaveUsuarioModal @emit-click="insertListner" />
+  
     <div class="container-list">
       <div class="container-header">
         <h1>Lista de Clientes</h1>
         <b-button class="title button-title" v-b-modal.modal-save>Adicionar</b-button>
 
-        <!-- <Button class="title button-title" message="Adicionar" path="/add-user"></Button> -->
       </div>
-      
       <div>
         <b-table :items="tableData" :fields="fields" striped responsive="sm" :busy="isBusy">
           <template v-slot:cell(opções)="row">
@@ -149,12 +64,11 @@
 <script>
 import Clientes from "../services/cliente";
 import Alert from "../components/utils/Alert";
-import Cidade from "../services/cidade";
-// import Button from "../components/utils/Button";
+import SaveUsuarioModal from "./modals/SaveUsuarioModal";
+import {mapActions} from 'vuex'
 export default {
   data() {
     return {
-      
       fields: [
         "id",
         "nome",
@@ -167,67 +81,30 @@ export default {
       ],
       isBusy: false,
       tableData: [],
-      editedItem: {
-        id: "",
-        nome: "",
-        cpfOuCnpj: "",
-        email: "",
-        tipo: null,
-        telefone: "",
-        logradouro: "",
-        numero: "99",
-        cep: "",
-        cidade_id: "1"
-      },
-      formUser: {
-        nome: "",
-        cpfOuCnpj: "",
-        email: "",
-        tipo: null,
-        telefone1: "",
-        logradouro: "",
-        numero: "99",
-        cep: "",
-        cidade_id: "1"
-      },
-      cidade_nome: "",
-
+      
       alertBody: {
         message: "",
         type: ""
       },
       visible: null,
-      titulo: "",
-      selected: "",
+      
+      
       tableDataCidade: [],
-      options: [
-        { item: 1, name: "Pessoa Física" },
-        { item: 2, name: "Pessoa Júridica" }
-      ]
+      
     };
   },
 
   name: "Clientes",
   components: {
     Alert,
+    SaveUsuarioModal
   },
 
   async created() {
     try {
       this.isBusy = true;
       const res = await Clientes.getAll();
-      const res1 = await Cidade.getAll();
-
-      res1.data.forEach(element => {
-        const temp = {
-          id: element.id,
-          nome: element.nome,
-          estado: element.estado.nome
-        };
-
-        this.tableDataCidade.push(temp);
-      });
-
+      
       if (Object.keys(res.data).length != 0) {
         res.data.forEach(element => {
           const temp = {
@@ -242,65 +119,50 @@ export default {
           this.tableData.push(temp);
         });
       } else {
-        const temp = {
-          message: "Nenhum cliente foi cadastrado",
-          type: "warning"
-        };
-
-        this.alertBody = temp;
-        this.visible = 5;
-
-        setTimeout(function() {
-          this.visible = null;
-        }, 5000);
+        this.showAlert("Nenhum cliente foi cadastrado ", "warning", null);
       }
     } catch (e) {
-      const temp = {
-        message: "Erro ao buscar no banco de dados: " + e.message,
-        type: "danger"
-      };
+      this.showAlert("Erro ao buscar no banco de dados", "danger", e.message);
 
-      this.alertBody = temp;
-      this.visible = 5;
-
-      setTimeout(function() {
-        this.visible = null;
-      }, 5000);
     }
     this.isBusy = false;
   },
   methods: {
+    ...mapActions([
+      'setTime'
+    ]),
     showDeleteModal(item) {
       this.deletedItem = item;
       this.$bvModal.show("delete-modal");
     },
 
-    showAlert(message, type, error) {
+    showAlert(msg, type, error) {
       const temp = {
-        message: message + ": " + error,
+        message: msg,
         type: type
       };
-
-      this.alertBody = temp;
-      this.visible = 5;
-
-      setTimeout(function() {
-        this.visible = null;
-      }, 5000);
-    },
-    async handleSave(){      
-      try{
-        const res = await Cidade.getId(this.cidade_nome)
-        
-        this.editedItem.cidade_id = res.data.id
-        
-        this.formUser.tipo = this.selected
-        await Clientes.post(this.formUser);
-        
-        this.tableData.push(this.formUser)
+      if (error != null) { 
+        temp.message = temp.message+": "+error
       }
-      catch(e){
-        alert(e)
+      this.alertBody = temp;
+      this.$store.dispatch("setTime", 5);
+    },
+    insertListner(item, response) {
+      if (response != null) {
+        this.showAlert("Item cadastrado com sucesso ! ", "primary", null);
+        const temp = {
+          id: response,
+          nome: item.nome,
+          email: item.email,
+          cpfOuCnpj: item.cpfOuCnpj,
+          telefone: item.telefone1,
+          logradouro: item.logradouro,
+          cep: item.cep
+        };
+        this.tableData.push(temp);
+
+      } else {
+        this.showAlert("Erro ao inserir item", "danger", item.message);
       }
     },
     async handleDelete() {
@@ -310,32 +172,15 @@ export default {
         await Clientes.del(this.deletedItem.id);
         const index = this.tableData.indexOf(this.deletedItem);
         this.tableData.splice(index, 1);
+        this.showAlert("Item deletado com sucesso !", "primary");
 
-        const temp = {
-          message: "Cliente deletado com sucesso !",
-          type: "primary"
-        };
-
-        this.alertBody = temp;
-        this.visible = 5;
-        setTimeout(function() {
-          this.visible = null;
-        }, 5000);
       } catch (e) {
-        const temp = {
-          message: "Erro ao deletar cliente: " + e.message,
-          type: "danger"
-        };
+        this.showAlert("Erro ao deletar item", "danger", e.message);
 
-        this.alertBody = temp;
-        this.visible = 5;
-        setTimeout(function() {
-          this.visible = null;
-        }, 5000);
       }
     },
 
-    async handleEdit(item) {
+    async handleEdit(item) {    
       const from = {
         id: item.id,
         nome: item.nome,
@@ -344,28 +189,10 @@ export default {
 
       try {
         await Clientes.put(from.id, from);
-
-        const temp = {
-          message: "Cliente atualizado com sucesso !",
-          type: "primary"
-        };
-
-        this.alertBody = temp;
-        this.visible = 5;
-        setTimeout(function() {
-          this.visible = null;
-        }, 5000);
+        this.showAlert("Item atualizado com sucesso !", "primary", null);
+        
       } catch (e) {
-        const temp = {
-          message: "Erro ao atualizar o cliente: " + e.message,
-          type: "danger"
-        };
-
-        this.alertBody = temp;
-        this.visible = 5;
-        setTimeout(function() {
-          this.visible = null;
-        }, 5000);
+        this.showAlert("Erro ao atualizar item", "danger", e.message);
       }
     }
   }
---
+++ front-spring/src/components/modals/SaveUsuarioModal.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+    <b-modal id="modal-save" title="Cadastrar" @ok="handleSave()">
+      <p class="my-4"></p>
+
+      <form>
+        <div class="form-group row">
+          <label for="inputEmail3" class="col-sm-2 col-form-label">Nome</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" v-model="saveItem.nome" />
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label for="inputEmail3" class="col-sm-2 col-form-label">CPF/CNPJ</label>
+          <div class="col-sm-10">
+            <input class="form-control" v-model="saveItem.cpfOuCnpj" />
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label for="inputPassword3" class="col-sm-2 col-form-label">Email</label>
+          <div class="col-sm-10">
+            <input class="form-control" v-model="saveItem.email" />
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label for="inputEmail3" class="col-sm-2 col-form-label">Telefone</label>
+          <div class="col-sm-10">
+            <input class="form-control" v-model="saveItem.telefone1" />
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label for="inputEmail3" class="col-sm-2 col-form-label">Cidade</label>
+          <div class="col-sm-10">
+            <b-form-input
+              placeholder="Selecione uma cidade"
+              list="my-list-id"
+              v-model="cidade_nome"
+            ></b-form-input>
+
+            <datalist id="my-list-id">
+              <option v-for="(cidade,index) in tableDataCidade" v-bind:key="index">{{ cidade.nome }}</option>
+            </datalist>
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label for="inputEmail3" class="col-sm-2 col-form-label">Logradouro</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" v-model="saveItem.logradouro" />
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label for="inputEmail3" class="col-sm-2 col-form-label">Cep</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" v-model="saveItem.cep" />
+          </div>
+        </div>
+
+        <fieldset class="form-group">
+          <div class="row">
+            <legend class="col-form-label col-sm-2 pt-0">Tipo</legend>
+            <div class="col-sm-10">
+              <div class="form-check">
+                <div>
+                  <b-form-radio-group
+                    v-model="selectedCity"
+                    :options="options"
+                    class="mb-3"
+                    value-field="item"
+                    text-field="name"
+                  ></b-form-radio-group>
+                </div>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+        <div class="form-group row">
+          <div class="col-sm-10"></div>
+        </div>
+      </form>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import Cidade from "@/services/cidade";
+import Clientes from "@/services/cliente"
+import { mapActions } from "vuex";
+export default {
+  name: "SaveCategoriaModal",
+  components: {},
+  data() {
+    return {
+      saveItem: {
+        nome: "",
+        cpfOuCnpj: "",
+        email: "",
+        tipo: null,
+        telefone1: "",
+        logradouro: "",
+        numero: "99",
+        cep: "",
+        cidade_id: ""
+      },
+      cidade_nome: "",
+
+      selectedCity: "",
+      tableDataCidade: [],
+      options: [
+        { item: 1, name: "Pessoa Física" },
+        { item: 2, name: "Pessoa Júridica" }
+      ],
+      alertBody: {
+        message: "",
+        type: ""
+      }
+    };
+  },
+  async created(){
+      const res = await Cidade.getAll();
+
+      res.data.forEach(element => {
+        const temp = {
+          id: element.id,
+          nome: element.nome,
+          estado: element.estado.nome
+        };
+
+        this.tableDataCidade.push(temp);
+      });
+  },
+  methods: {
+    ...mapActions(["setTime"]),
+    async handleSave() {
+        try{
+        const res = await Cidade.getId(this.cidade_nome)
+
+        this.saveItem.cidade_id = res.data.id
+        this.saveItem.tipo = this.selectedCity
+        
+        const response = await Clientes.post(this.saveItem);
+        this.$emit("emit-click", this.saveItem, response.data);
+        
+      }
+      catch(e){
+        this.$emit("emit-click", e, null);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
