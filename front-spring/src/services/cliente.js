f5ee264418b0a8f8d949be4ebdecb55003175387
Pedro <p.paiva.pdo@gmail.com>
2020-05-28 17:35:10 -0300

    implementado servico de usuario logado

--- front-spring/src/services/cliente.js
+++ front-spring/src/services/cliente.js
@@ -19,10 +19,37 @@ const del = (id) => {
 const put = (id,data) => {
     return Api().put(`/clientes/${id}`,data)
 }
+
+const getUserAutenticated = () => {
+    let token = localStorage.getItem('token')
+    const config = {
+        headers: {
+            Authorization: "Bearer "+token
+        }
+    }
+    return Api().get('clientes/autenticated', config)
+}
+
+const postPicture = (data) => {
+    let token = localStorage.getItem('token')
+    let formData = new FormData();
+    formData.append('file', data)
+
+    const config = {
+        headers: {
+            "content-Type": "multipart/form-data",
+            Authorization: "Bearer "+token
+        }
+    }
+
+    return Api().post('/clientes/picture', formData, config)
+}
 export default {
     getAll,
     getOne,
     post,
     del,
-    put
+    put,
+    postPicture,
+    getUserAutenticated
 }
