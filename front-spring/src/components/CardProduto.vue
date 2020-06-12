e3c7c17bec00dc8b56d6ab7bfba5362c29f983b0
Pedro <p.paiva.pdo@gmail.com>
2020-06-12 20:33:35 -0300

    card produto modificado

--- front-spring/src/components/CardProduto.vue
+++ front-spring/src/components/CardProduto.vue
@@ -14,22 +14,6 @@
         </router-link>
       </div>
     </div>
-
-    <!-- <b-card
-      :img-src="imageUrl"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem;"
-      class="mb-2"
-    >
-      <b-card-title class="nome-produto">{{nomeProduto}}</b-card-title> -->
-      <!-- <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text> -->
-      <!-- <h4 class="preco-produto">{{precoProduto}}</h4>
-      <h5 class="descricao">12X de 74,91 sem juros</h5>
-
-      <b-button href="#" variant="primary">Carrinho</b-button>
-    </b-card> -->
   </div>
 </template>
 
