45d9929042e2e6d91e0b52afa68dda0e6229bd8a
Pedro <p.paiva.pdo@gmail.com>
2020-07-02 17:11:47 -0300

    anotacao service email, problema heroku

--- exemple/src/main/java/com/example/curso/exemple/service/EmailService.java
+++ exemple/src/main/java/com/example/curso/exemple/service/EmailService.java
@@ -4,9 +4,11 @@ import com.example.curso.exemple.domain.Cliente;
 import com.example.curso.exemple.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.stereotype.Service;
 
 import javax.mail.internet.MimeMessage;
 
+@Service
 public interface EmailService {
 
     void sendOrderComfirmationEmail(Pedido obj);
